ed634dbdc61ff9cbf43b65bd427d766f186fc764
louisdelange2-max <louisdelange2@gmail.com>
2026-05-21 19:09:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='earthlabs-drilling-data-captured-v1';
+const CACHE_NAME='earthlabs-drilling-data-captured-v2';
 const ASSETS=['./','./index.html','./manifest.json','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
